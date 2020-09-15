6055f953633593f8fad89e2da277bcbe97ad35ee
Abhir Raj Shrivastava <56101022+Abhir-24@users.noreply.github.com>
2020-09-15 21:39:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,30 @@ $(document).ready(function() {
       $("#menu-btn").toggleClass("turn");
     }
   });
+
+
+  $(document).ready(function() {
+    let number = parseInt($('.pull').text(), 10) || 0; 
+
+
+    let interval = setInterval(function() {
+        $('.pull').text(number++); 
+
+        if (number > 100000) {
+            clearInterval(interval); 
+        }
+    }, 300); 
+}); 
+
+
+$(document).ready(function() {
+  let number2 = parseInt($('.comp').text(), 10) || 0; 
+
+  let interval2 = setInterval(function() {
+      $('.comp').text(number2++); 
+
+      if (number2 > 100000) {
+          clearInterval(interval2); 
+      }
+  }, 200); 
+}); 
